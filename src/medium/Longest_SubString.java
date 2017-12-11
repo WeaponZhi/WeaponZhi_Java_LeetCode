d59dfc0d612d2b9fe6b140e070a6b6232b927400
WeaponZhi <584098488@qq.com>
2017-12-11 22:41:31 +0800

    add well solution for Longest_SubString

--- src/medium/Longest_SubString.java
+++ src/medium/Longest_SubString.java
@@ -1,6 +1,8 @@
 package medium;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Given a string, find the length of the longest substring without repeating characters.
@@ -33,6 +35,23 @@ public class Longest_SubString {
         return longestCount;
     }
 
+    public int wellSolution(String s){
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Longest_SubString test = new Longest_SubString();
         System.out.println(test.myLengthOfLongestSubstring("abba"));
@@ -41,5 +60,7 @@ public class Longest_SubString {
         System.out.println(test.myLengthOfLongestSubstring("abcabcbb"));
         System.out.println(test.myLengthOfLongestSubstring("bbbbb"));
         System.out.println(test.myLengthOfLongestSubstring("pwwkew"));
+
+        System.out.println(test.wellSolution("dsvd"));
     }
 }
